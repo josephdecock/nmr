6193d16b066abf87d544540924c52f2324283800
Kyle Kalstabakken <kyle.kalstabakken@gmail.com>
2011-12-19 23:16:22 -0600

    Refactor plotData() as a method of Data (Data.plot).

--- js/data.js
+++ js/data.js
@@ -3,6 +3,32 @@ function Data() {
     this.params = new Object;
 }
 
+Data.prototype.plot = function() {
+    var svg = $('#svg');
+    // Set svg canvas size to that of the available space in the window
+    // It is not clear at this point where the extra 5 pixels come from
+    svg.height($(window).height() - $('.menu').outerHeight() - 5);
+    svg.width($(window).width());
+    $('#plot').height($(window).height() - $('.menu').outerHeight - 5);
+    if (!this.spectrum) {
+        this.spectrum = document.createElementNS('http://www.w3.org/2000/svg',
+                                                 'polyline');
+        this.spectrum.setAttributeNS(null, 'fill', 'none');
+        this.spectrum.setAttributeNS(null, 'stroke', 'blue');
+        this.spectrum.setAttributeNS(null, 'stroke-width', '5');
+        this.spectrum.setAttributeNS(null, 'points', data.plotPoints.join(' '));
+        svg.append(this.spectrum);
+    }
+    // Puts the X-axis in the middle of the plot. Should only be used for FIDs
+    var translateY = svg.innerHeight() / 2;
+    // Scale the plot down to fit onto the SVG canvas
+    var scaleX = svg.innerWidth() / data.rData.length;
+    var scaleY = svg.innerHeight() / (2 * data.maxAbsPoint);
+    this.spectrum.setAttributeNS(null, 'transform',
+                        'translate(0 ' + translateY + ') ' +
+                        'scale(' + scaleX + ' ' + scaleY + ')');
+}
+
 Data.prototype.ft = function() {
 // Do DFT
 }
--- js/processor.js
+++ js/processor.js
@@ -22,10 +22,10 @@ function initProcessor() {
     for (var i = 1; i <= data.rData.length; i++) {
         data.plotPoints.push(i + ',' + data.rData[i - 1]);
     }
-    plotData(true);
+    data.plot();
     $(window).resize(function() {
         // Resize plot when window is resized
-        plotData();
+        data.plot();
     });
 }
 
@@ -36,32 +36,4 @@ function showMenu(event, button) {
         $('.list').hide();
         $(this).find('.list').show();
     }, null);
-}
-
-function plotData(newData) {
-    var svg = $('#svg');
-    // Set svg canvas size to that of the available space in the window
-    // It is not clear at this point where the extra 5 pixels come from
-    svg.height($(window).height() - $('.menu').outerHeight() - 5);
-    svg.width($(window).width());
-    $('#plot').height($(window).height() - $('.menu').outerHeight - 5);
-    if (!data.plot) {
-        data.plot = document.createElementNS('http://www.w3.org/2000/svg',
-                                                 'polyline');
-        data.plot.setAttributeNS(null, 'fill', 'none');
-        data.plot.setAttributeNS(null, 'stroke', 'blue');
-        data.plot.setAttributeNS(null, 'stroke-width', '5');
-        svg.append(data.plot);
-    }
-    if (newData) {
-        data.plot.setAttributeNS(null, 'points', data.plotPoints.join(' '));
-    }
-    // Puts the X-axis in the middle of the plot. Should only be used for FIDs
-    var translateY = svg.innerHeight() / 2;
-    // Scale the plot down to fit onto the SVG canvas
-    var scaleX = svg.innerWidth() / data.rData.length;
-    var scaleY = svg.innerHeight() / (2 * data.maxAbsPoint);
-    data.plot.setAttributeNS(null, 'transform',
-                        'translate(0 ' + translateY + ') ' +
-                        'scale(' + scaleX + ' ' + scaleY + ')');
 }
